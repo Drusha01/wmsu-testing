89b265f8ba0196120a4e97ebbd64cd41d8fa8e3f
salialkhayzel <111584408+salialkhayzel@users.noreply.github.com>
2023-08-31 23:34:50 +0800

    test-application
    user test applying >

--- resources/views/test-application/Cet.blade.php
+++ resources/views/test-application/Cet.blade.php
@@ -2,97 +2,75 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('css/test-application.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>College Entrance Exam Application</title>
-    <link rel="stylesheet" href="{{ asset('css/test-application.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-
-    <style>
-        body {
-            background-color: #f8f9fa;
-        }
-        .container {
-            margin-top: 50px;
-            max-width: 800px;
-            background-color: #ffffff;
-            padding: 30px;
-            border-radius: 10px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-        }
-    </style>
 </head>
-
-<body>
-    <div class="container">
-        <h2 class="mb-4">College Entrance Exam Application</h2>
-        <form>
-            <fieldset class="mb-4">
-                <legend>Personal Information</legend>
-                <div class="row">
-                    <div class="col-md-4 mb-3">
-                        <label for="first-name">First Name:</label>
-                        <input type="text" id="first-name" class="form-control" required>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="middle-name">Middle Name:</label>
-                        <input type="text" id="middle-name" class="form-control">
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="last-name">Last Name:</label>
-                        <input type="text" id="last-name" class="form-control" required>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6 mb-3">
-                        <label for="address">Address:</label>
-                        <input id="address" class="form-control" rows="3" required></input>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="email">Email:</label>
-                        <input type="email" id="email" class="form-control" required>
-                    </div>
-                </div>
-            </fieldset>
-            <fieldset class="mb-4">
-                <legend>Academic Information</legend>
-                <div class="mb-3">
-                    <label for="school-name">Senior High School Name:</label>
-                    <input type="text" id="school-name" class="form-control" required>
-                </div>
-                <div class="mb-3">
-                    <label for="school-address">Senior High School Address:</label>
-                    <input id="school-address" class="form-control" rows="3" required></input>
-                </div>
-            </fieldset>
-            <fieldset class="mb-4">
-                <legend>Document Uploads</legend>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="senior-card-original">Original Senior High School Card:</label>
-                            <input type="file" id="senior-card-original" class="form-control-file" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="medical-certification">Medical Certification:</label>
-                            <input type="file" id="medical-certification" class="form-control-file" required>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="barangay-clearance">Barangay Clearance:</label>
-                            <input type="file" id="barangay-clearance" class="form-control-file" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="photo">Two Identical 2x2 Photos with Name Tag:</label>
-                            <input type="file" id="photo" class="form-control-file" required>
-                        </div>
-                    </div>
-                </div>
-            </fieldset>
-            <div class="text-center">
-                <button type="submit" class="btn btn-primary">Submit Application</button>
-            </div>
-        </form>
+<body class="body-form2">
+<div class="form2-container">
+    <div class="form2-header">
+        <img src="{{ asset('images/logo/logo.png') }}" alt="Logo" class="form2-logo">
+        <div class="form2-header-text">
+            <span>Western Mindanao State University</span>
+            <h2 class="mb-4">College Entrance Exam Application Form</h2>
+        </div>
     </div>
+    <form method="POST" action="{{ url('application.submit') }}" enctype="multipart/form-data">
+        @csrf
+        <fieldset class="mb-4">
+            <div class="row mt-5">
+                <div class="col-md-4 mb-3">
+                    <label for="first-name">First Name:</label>
+                    <input type="text" id="first-name" name="first_name" class="form-control" required>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <label for="middle-name">Middle Name:</label>
+                    <input type="text" id="middle-name" name="middle_name" class="form-control">
+                </div>
+                <div class="col-md-4 mb-3">
+                    <label for="last-name">Last Name:</label>
+                    <input type="text" id="last-name" name="last_name" class="form-control" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label for="address">Address:</label>
+                    <textarea id="address" name="address" class="form-control" rows="3" required></textarea>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" class="form-control" required>
+                </div>
+            </div>
+        </fieldset>
+        <fieldset class="mb-4">
+            <legend>Document Uploads</legend>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="senior-card-original">Original Senior High School Card:</label>
+                        <input type="file" id="senior-card-original" name="senior_card_original" class="form-control-file" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="medical-certification">Medical Certification:</label>
+                        <input type="file" id="medical-certification" name="medical_certification" class="form-control-file" required>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="barangay-clearance">Barangay Clearance:</label>
+                        <input type="file" id="barangay-clearance" name="barangay_clearance" class="form-control-file" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="photo">Two Identical 2x2 Photos with Name Tag:</label>
+                        <input type="file" id="photo" name="photo" class="form-control-file" required>
+                    </div>
+                </div>
+            </div>
+        </fieldset>
+        <button type="submit" class="submit-button">Submit Application</button>
+    </form>
+</div>
 </body>
 </html>
--- resources/views/test-application/Gsat.blade.php
+++ resources/views/test-application/Gsat.blade.php
@@ -1,81 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
-    
 <head>
 <meta charset="UTF-8">
+<link rel="stylesheet" href="{{ asset('css/test-application.css') }}">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>GSAT Application Form</title>
-<link rel="stylesheet" href="{{ asset('css/test-application.css') }}">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-<style>
-  body {
-    background-color: #f8f9fa;
-  }
-  .container {
-    margin-top: 50px;
-    max-width: 600px;
-    background-color: #ffffff;
-    padding: 30px;
-    border-radius: 10px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-  }
-</style>
 </head>
-
-<body>
-
-<div class="container">
-  <h2 class="mb-4">GSAT Application Form</h2>
-  <form>
-    <div class="row mb-3">
-      <div class="col">
-        <label for="firstName" class="form-label">First Name</label>
-        <input type="text" class="form-control" id="firstName" name="firstName" required>
-      </div>
-      <div class="col">
-        <label for="middleName" class="form-label">Middle Name</label>
-        <input type="text" class="form-control" id="middleName" name="middleName">
-      </div>
-      <div class="col">
-        <label for="lastName" class="form-label">Last Name</label>
-        <input type="text" class="form-control" id="lastName" name="lastName" required>
-      </div>
-    </div>
-    <div class="mb-3">
-      <label for="email" class="form-label">Email</label>
-      <input type="email" class="form-control" id="email" name="email" required>
-    </div>
-    <div class="mb-3">
-      <label for="address" class="form-label">Address</label>
-      <textarea class="form-control" id="address" name="address" rows="3" required></textarea>
-    </div>
-    <div class="mb-3">
-      <label class="form-label">Document Uploads</label>
-      <div class="row">
-        <div class="col">
-           <input type="file" class="form-control" id="tor" name="tor" accept=".pdf,.doc,.docx,.jpg,.png,.jpeg" required>
-          <label for="bachelorDegree">Bachelor Degree (Original)</label>
+<body class="gsat-body">
+<div class="gsat-container">
+    <div class="gsat-header">
+        <img src="{{ asset('images/logo/logo.png') }}" alt="Logo" class="gsat-logo">
+        <div class="header-eat-text">
+            <span>Western Mindanao State University</span>
+            <h3>GRADUATE SCHOOL ADMISSION TEST (GSAT)</h3>
         </div>
-        <div class="col">
-          <input type="file" class="form-control" id="tor" name="tor" accept=".pdf,.doc,.docx,.jpg,.png,.jpeg" required>
-          <label for="tor">TOR (Original)</label>
+    </div>
+    <form method="POST" action="{{ url('application.submit') }}" enctype="multipart/form-data">
+        @csrf
+        <div class="row mb-3 mt-4">
+            <div class="col">
+                <label for="firstName" class="form-label">First Name</label>
+                <input type="text" class="form-control" id="firstName" name="first_name" required>
+            </div>
+            <div class="col">
+                <label for="middleName" class="form-label">Middle Name</label>
+                <input type="text" class="form-control" id="middleName" name="middle_name">
+            </div>
+            <div class="col">
+                <label for="lastName" class="form-label">Last Name</label>
+                <input type="text" class="form-control" id="lastName" name="last_name" required>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="mb-3">
-      <label class="form-label">2x2 Photo with Name Tag</label>
-      <input type="file" class="form-control" id="photo" name="photo" accept="image/*" required>
-    </div>
-    <div class="mb-3">
-      <label class="form-label">Barangay Clearance</label>
-      <input type="file" class="form-control" id="tor" name="tor" accept=".pdf,.doc,.docx,.jpg,.png,.jpeg" required>
-    </div>
-    <div class="mb-3">
-      <label class="form-label">Medical Certificate</label>
-      <input type="file" class="form-control" id="tor" name="tor" accept=".pdf,.doc,.docx,.jpg,.png,.jpeg" required>
-    <button type="submit" class="btn btn-primary mt-2">Submit Application</button>
-  </form>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <div class="mb-3">
+            <label for="address" class="form-label">Address</label>
+            <textarea class="form-control" id="address" name="address" rows="3" required></textarea>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Document Uploads</label>
+            <div class="row">
+                <div class="col">
+                    <input type="file" class="form-control" id="bachelorDegree" name="bachelor_degree" accept=".pdf,.doc,.docx,.jpg,.png,.jpeg" required>
+                    <label for="bachelorDegree">Bachelor Degree (Original)</label>
+                </div>
+                <div class="col">
+                    <input type="file" class="form-control" id="tor" name="tor" accept=".pdf,.doc,.docx,.jpg,.png,.jpeg" required>
+                    <label for="tor">TOR (Original)</label>
+                </div>
+            </div>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">2x2 Photo with Name Tag</label>
+            <input type="file" class="form-control" id="photo" name="photo" accept="image/*" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Barangay Clearance</label>
+            <input type="file" class="form-control" id="barangayClearance" name="barangay_clearance" accept=".pdf,.doc,.docx,.jpg,.png,.jpeg" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Medical Certificate</label>
+            <input type="file" class="form-control" id="medicalCertificate" name="medical_certificate" accept=".pdf,.doc,.docx,.jpg,.png,.jpeg" required>
+        </div>
+        <button type="submit" class="submit-button">Submit Application</button>
+    </form>
 </div>
-
 </body>
 </html>
