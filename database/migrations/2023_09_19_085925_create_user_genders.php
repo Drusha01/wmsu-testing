3ac22e4c2c0009e2533f657588efe6cf8a6c43ab
Hanrickson E. Dumapit <hanz.dumapit53@gmail.com>
2023-09-19 17:12:06 +0800

    user_db initial

--- database/migrations/2023_09_19_085925_create_user_genders.php
+++ database/migrations/2023_09_19_085925_create_user_genders.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUserGenders extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        DB::statement('CREATE TABLE user_genders(
+            user_genders_id INT PRIMARY KEY AUTO_INCREMENT,
+            user_genders_details VARCHAR(100) UNIQUE,
+            created_at DATETIME ,
+            updated_at DATETIME 
+        );');
     }
 
     /**
@@ -26,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_genders');
     }
 }
--- database/migrations/2023_09_19_085936_create_user_sex.php
+++ database/migrations/2023_09_19_085936_create_user_sex.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserRolesTable extends Migration
+class CreateUserSex extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateUserRolesTable extends Migration
      */
     public function up()
     {
-        DB::statement('CREATE TABLE user_roles(
-            user_role_id INT PRIMARY KEY AUTO_INCREMENT,
-            user_role_details VARCHAR(100) UNIQUE,
+        DB::statement('CREATE TABLE user_sex(
+            user_sex_id INT PRIMARY KEY AUTO_INCREMENT,
+            user_sex_details VARCHAR(100) UNIQUE,
             created_at DATETIME ,
             updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
         );');
@@ -28,6 +28,6 @@ class CreateUserRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_roles');
+        Schema::dropIfExists('user_sex');
     }
 }
