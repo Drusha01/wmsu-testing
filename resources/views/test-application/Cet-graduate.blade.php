c1d6147cc343b0ad21b1ccbf5b5b815302f450e7
Hanrickson E. Dumapit <hanz.dumapit53@gmail.com>
2023-10-07 01:04:38 +0800

    css bug fix

--- resources/views/test-application/Cet-graduate.blade.php
+++ resources/views/test-application/Cet-graduate.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="{{ asset('css/Test-application.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/test-application.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CET SHS Graduated Form</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
