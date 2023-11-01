1d0ef61ca9982e6da3af6b14e0054b87105b507d
MattLuna04 <xt202002170@wmsu.edu.ph>
2023-11-02 01:16:59 +0800

    application

--- resources/views/livewire/components/header/header-navigation2.blade.php
+++ resources/views/livewire/components/header/header-navigation2.blade.php
@@ -17,14 +17,14 @@
                 </button>
                 <!-- Navigation links -->
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mx-auto">
-                        <li class="nav-item">
+                    <ul class="navbar-nav mx-auto px-2">
+                        <li class="nav-item ">
                             <a class="nav-link" href="{{ route('home') }}">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('about') }}">About Us</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item ">
                             <a class="nav-link" href="{{ route('services') }}">Services</a>
                         </li>
                         <li class="nav-item">
