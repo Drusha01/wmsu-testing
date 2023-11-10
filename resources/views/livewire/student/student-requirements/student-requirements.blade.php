1704fe90dab1f5c804e1d29c1f19900a1619b9eb
MattLuna04 <xt202002170@wmsu.edu.ph>
2023-11-11 02:59:28 +0800

    REQUIREMENTS

--- resources/views/livewire/student/student-requirements/student-requirements.blade.php
+++ resources/views/livewire/student/student-requirements/student-requirements.blade.php
@@ -16,39 +16,14 @@
             <div class="tab-pane fade show active " id="requirements-upload">
             <div class="container py-4">
       <div class="border border-dark px-2 mt-2" style="background-color: #ddd;">
-        <h2 class="text-danger">APPLICATION FOR ADMISSION FOR SY 2023-2024</h2>
+        <h2 class="text-danger">REQUIREMENTS FOR ADMISSION </h2>
       </div>
       <div class="border border-dark px-2">
-        <p class="text-danger font-weight-bold">I. MINIMUM APPLICATION REQUIREMENTS</p>
-        <ol type="1">
-          <li>Senior high school student expecting to graduate before SY 2023-2024 from a DepEd-accredited high school or an accredited secondary school abroad; OR</li>
-          <li>Graduate of a DepEd-accredited high school or accredited secondary school abroad; AND</li>
-          <li>Must have final grades for Grade 8, 9, 10 and Grade 11;</li>
-          <li>Must not have taken any college subject/s;</li>
-          <li>Must not have taken the UP College Admission Test in the AY 2020-2021 intake or earlier; AND</li>
-        </ol>
+       
+        
         <div>
-          <h4 class="text-danger">DATES TO REMEMBER</h4>
-          <div class="container w-75">
-            <div class="row border border-dar">
-              <div class="col border border-dar" style="font-size: 20px;">START OF ONLINE APPLICATION PERIOD</div>
-              <div class="col border border-dar" style="font-size: 20px;">15 September 2022</div>
-              <div class="w-100"></div>
-              <div class="col border border-dar" style="font-size: 20px;">START OF ONLINE APPLICATION PERIOD</div>
-              <div class="col border border-dar" style="font-size: 20px;">15 September 2022</div>
-            </div>
-          </div>
-        </div>
-        <div>
-          <p class="text-danger font-weight-bold">II. LIST OF REQUIRED DOCUMENTS</p>
-          <table class="table table-bordered w-80">
-            <thead>
-              <tr>
-                <th scope="col"> CATEGORY OF APPLICANT  </th>
-                <th scope="col">REQUIRED DOCUMENTS</th>
-              </tr>
-            </thead>
-            <tbody>
+          <p class="text-danger font-weight-bold">I. LIST OF REQUIRED DOCUMENTS</p>
+          
             <table class="table table-bordered w-80">
                                         <thead>
                                           <tr>
@@ -134,12 +109,7 @@
             </tbody>
           </table>
         </div>
-        <div>
-          <p class="text-danger font-weight-bold">III. HOW TO APPLY</p>
-          <h5 class="text-danger"> Fill out application Form. Online application opens by September 15, 2022. </h5>
-          <p>For Online Application Go To: <a href="https://wmsutec.online/student/application">wmsutec.online </a> ( you must login/register before you can apply)</p>
-          <h5 class="text-danger">  </h5>
-        </div>
+        
       </div>
       </div>
       </div>
