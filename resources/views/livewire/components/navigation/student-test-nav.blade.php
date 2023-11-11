9603ce24d7e588596bf6c9c6c5b8194567af339a
salialkhayzel <111584408+salialkhayzel@users.noreply.github.com>
2023-11-11 18:47:30 +0800

    inform

--- resources/views/livewire/components/navigation/student-test-nav.blade.php
+++ resources/views/livewire/components/navigation/student-test-nav.blade.php
@@ -38,7 +38,7 @@
         <!-- Other Tests Group -->
         <div class="card">
             <div class="card-header">
-                Other Tests
+                Other Examination Form
             </div>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item d-flex justify-content-between align-items-center ">
--- resources/views/livewire/student/student-application/student-application.blade.php
+++ resources/views/livewire/student/student-application/student-application.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div role="tabpanel" class="tab-pane" id="application ">
+    <div role="tabpanel" class="tab-pane" id="application">
         <section class="exam-reminders-section">
             <div class="container">
                 <div class="row">
@@ -7,21 +7,21 @@
 
                     <div class="col-lg-6 guide mt-5">
                         <div class="guide-section mt-5">
-                            <h3>Reminders for WMSU Exam Takers</h3>
-                            <p>Here are important reminders for WMSU exam takers:</p>
+                            <h3>Reminders for WMSU Applicant</h3>
+                            <p>Here are important reminders:</p>
                             <ul class="reminder-list">
                                 <li>You can only apply CET once.</li>
-                                <li style="color:red; font-weight:bold;">Please settle your payment on the Registar cashier as it will be a part of requirements to be uploaded in your form if you already taken CET once.</li>
+                                <li style="color:red; font-weight:bold;">Please settle your payment on the Registrar cashier as it will be a part of requirements to be uploaded in your form if you already took CET once.</li>
                                 <li>Select the specific test you wish to take.</li>
-                                <li>Upload all required documents before the exam date.</li>
-                                <li>Submit your application on time.</li>
+                                <li>Ensure that all information provided in the application form is accurate and up-to-date.</li>
+                                <li>Double-check filling up the needed information</li>
+                                <li>Prepare all necessary identification documents, pictures, etc.</li>
+                                <li>If you have any questions or need assistance, please don't hesitate to contact our support team.</li>
                             </ul>
-                            <p>If you have any questions or need assistance, please don't hesitate to contact our support team.</p>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
     </div>
-
 </div>
