7355ec0f45d17569770db1670cd01f07120cfcb7
salialkhayzel <111584408+salialkhayzel@users.noreply.github.com>
2023-11-16 01:04:19 +0800

    announcement

--- resources/views/livewire/student/student-announcement/student-announcement.blade.php
+++ resources/views/livewire/student/student-announcement/student-announcement.blade.php
@@ -2,21 +2,24 @@
     <div role="tabpanel" class="tab-pane" id="schedule">
         <section class="schedule-section">
             <div class="container">
-                <h2 class="section-title"> Announcement</h2>
+                <h2 class="section-title text-center">Announcement</h2>
                 <ul class="exam-list">
-                    <li class="exam-item">
+                    <li class="exam-item text-center">
                         <h3 class="exam-title">College Entrance Test (CET)</h3>
                         <p class="exam-date">Date: October 10, 2023</p>
+                        <img src="{{ asset('images/about/about.jpg') }}" alt="CET Announcement Image" style="width: 600px; height: 350px; object-fit: cover;"  class="img-thumbnail">
                         <p class="exam-description">The College Entrance Test (CET) will be held on the specified date. Make sure to prepare!</p>
                     </li>
-                    <li class="exam-item">
+                    <li class="exam-item text-center">
                         <h3 class="exam-title">Nursing Aptitude Test (NAT)</h3>
                         <p class="exam-date">Date: November 15, 2023</p>
+                        <img src="announcement_image2.jpg" alt="NAT Announcement Image" class="img-thumbnail">
                         <p class="exam-description">Get ready for the Nursing Aptitude Test (NAT) coming up on the given date.</p>
                     </li>
-                    <li class="exam-item">
+                    <li class="exam-item text-center">
                         <h3 class="exam-title">Graduate School Admission Test (GSAT)</h3>
                         <p class="exam-date">Date: December 5, 2023</p>
+                        <img src="announcement_image3.jpg" alt="GSAT Announcement Image" class="img-thumbnail">
                         <p class="exam-description">The Graduate School Admission Test (GSAT) is scheduled for the specified date. Don't miss it!</p>
                     </li>
                     <!-- Add more exam items as needed -->
