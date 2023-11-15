a1f2471ebf95de774faa856d1aa26ecaf7e0ffa9
salialkhayzel <111584408+salialkhayzel@users.noreply.github.com>
2023-11-16 00:36:27 +0800

    notifications

--- resources/views/livewire/admin/notification.blade.php
+++ resources/views/livewire/admin/notification.blade.php
@@ -1,3 +1,4 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 <div>
     <x-loading-indicator/>
     <main id="main" class="main">
@@ -20,33 +21,269 @@
                 </li>
             </ul>
             <div class="tab-content">
-                <!-- Tab 1: All Notifications -->
-                <div class="tab-pane fade show active" id="allNotifications">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Title</th>
-                                <th scope="col">Message</th>
-                                <th scope="col">Time</th>
-                            </tr>
-                        </thead>
-                        <tbody id="notification-content">
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Avail</td>
-                                <td>Sali, Khay Abdilla has availed gym-use.</td>
-                                <td>2023-09-18 14:25:57</td>
-                            </tr>
-                            <!-- Add more notification items here -->
-                        </tbody>
-                    </table>
-                </div>
+
+            <!-- Tab 1: All Notifications -->
+            <div class="tab-pane fade show active" id="allNotifications">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th scope="col">Title</th>
+                            <th scope="col"><i class="far fa-comment"></i></th>
+                            <th scope="col"><i class="far fa-clock"></i></th>
+                        </tr>
+                    </thead>
+                    <tbody id="notification-content">
+                        <tr>
+                            <!-- <th scope="row">1</th> -->
+                            <td><i class="fas fa-calendar-check"></i></td>
+                            <td>Appointment Scheduled</td>
+                            <td>An appointment for John Doe has been scheduled for CET testing.</td>
+                            <td>2023-09-18 14:25:57</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-file-alt"></i></td>
+                            <td>Online Application Submitted</td>
+                            <td>Student ID 12345 has submitted an online CET application.</td>
+                            <td>2023-09-19 09:30:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-cogs"></i></td>
+                            <td>Application Processing</td>
+                            <td>An application is currently being processed for CET.</td>
+                            <td>2023-09-20 11:45:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-user-cog"></i></td>
+                            <td>Account Update</td>
+                            <td>Account information for Jane Smith has been updated.</td>
+                            <td>2023-09-21 15:00:00</td>
+                        </tr>
+                        <!-- Add more notification items -->
+                        <tr>
+                            <td><i class="fas fa-calendar-check"></i></td>
+                            <td>Appointment Rescheduled</td>
+                            <td>Appointment for Mary Johnson has been rescheduled.</td>
+                            <td>2023-09-22 16:30:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-file-alt"></i></td>
+                            <td>New Online Application</td>
+                            <td>New application received from Student ID 67890 for CET.</td>
+                            <td>2023-09-23 09:45:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-cogs"></i></td>
+                            <td>Application Review</td>
+                            <td>An application is pending review for CET.</td>
+                            <td>2023-09-24 12:10:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-user-cog"></i></td>
+                            <td>Account Deactivated</td>
+                            <td>Account for Mark Lee has been deactivated.</td>
+                            <td>2023-09-25 14:20:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-calendar-check"></i></td>
+                            <td>New Appointment Request</td>
+                            <td>New appointment requested for CET testing.</td>
+                            <td>2023-09-26 17:00:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-file-alt"></i></td>
+                            <td>Application Denied</td>
+                            <td>Application from Student ID 24680 has been denied for CET.</td>
+                            <td>2023-09-27 10:30:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-cogs"></i></td>
+                            <td>Application Approved</td>
+                            <td>An application has been approved for CET.</td>
+                            <td>2023-09-28 13:15:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-user-cog"></i></td>
+                            <td>New Account Created</td>
+                            <td>New account created for Sarah Johnson.</td>
+                            <td>2023-09-29 11:40:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-calendar-check"></i></td>
+                            <td>Appointment Cancelled</td>
+                            <td>An appointment for CET testing has been cancelled.</td>
+                            <td>2023-09-30 09:00:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-file-alt"></i></td>
+                            <td>Online Application Updated</td>
+                            <td>Student ID 13579 has updated their online application for CET.</td>
+                            <td>2023-10-01 14:50:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-cogs"></i></td>
+                            <td>Application Review Complete</td>
+                            <td>Application review process completed for CET.</td>
+                            <td>2023-10-02 16:20:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-user-cog"></i></td>
+                            <td>Password Reset</td>
+                            <td>Password reset request for John Smith's account.</td>
+                            <td>2023-10-03 10:10:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-calendar-check"></i></td>
+                            <td>Appointment Completed</td>
+                            <td>Appointment for CET testing has been successfully completed.</td>
+                            <td>2023-10-04 13:00:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-file-alt"></i></td>
+                            <td>Online Application Reviewed</td>
+                            <td>Student ID 56789's application reviewed for CET.</td>
+                            <td>2023-10-05 11:05:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-cogs"></i></td>
+                            <td>Account Activated</td>
+                            <td>Account activation for Emily White's account.</td>
+                            <td>2023-10-06 14:30:00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
                 <!-- Tab 2: Unread Notifications -->
                 <div class="tab-pane fade" id="unreadNotifications">
-                    <h2>Unread Notifications</h2>
-                    <p>Welcome to the Unread Notifications tab. Here, you can view all your unread notifications.</p>
-                    <!-- Add more unread notification items here -->
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th scope="col">Title</th>
+                            <th scope="col"><i class="far fa-comment"></i></th>
+                            <th scope="col"><i class="far fa-clock"></i></th>
+                        </tr>
+                    </thead>
+                    <tbody id="notification-content">
+                        <tr>
+                            <!-- <th scope="row">1</th> -->
+                            <td><i class="fas fa-calendar-check"></i></td>
+                            <td>Appointment Scheduled</td>
+                            <td>An appointment for John Doe has been scheduled for CET testing.</td>
+                            <td>2023-09-18 14:25:57</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-file-alt"></i></td>
+                            <td>Online Application Submitted</td>
+                            <td>Student ID 12345 has submitted an online CET application.</td>
+                            <td>2023-09-19 09:30:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-cogs"></i></td>
+                            <td>Application Processing</td>
+                            <td>An application is currently being processed for CET.</td>
+                            <td>2023-09-20 11:45:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-user-cog"></i></td>
+                            <td>Account Update</td>
+                            <td>Account information for Jane Smith has been updated.</td>
+                            <td>2023-09-21 15:00:00</td>
+                        </tr>
+                        <!-- Add more notification items -->
+                        <tr>
+                            <td><i class="fas fa-calendar-check"></i></td>
+                            <td>Appointment Rescheduled</td>
+                            <td>Appointment for Mary Johnson has been rescheduled.</td>
+                            <td>2023-09-22 16:30:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-file-alt"></i></td>
+                            <td>New Online Application</td>
+                            <td>New application received from Student ID 67890 for CET.</td>
+                            <td>2023-09-23 09:45:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-cogs"></i></td>
+                            <td>Application Review</td>
+                            <td>An application is pending review for CET.</td>
+                            <td>2023-09-24 12:10:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-user-cog"></i></td>
+                            <td>Account Deactivated</td>
+                            <td>Account for Mark Lee has been deactivated.</td>
+                            <td>2023-09-25 14:20:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-calendar-check"></i></td>
+                            <td>New Appointment Request</td>
+                            <td>New appointment requested for CET testing.</td>
+                            <td>2023-09-26 17:00:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-file-alt"></i></td>
+                            <td>Application Denied</td>
+                            <td>Application from Student ID 24680 has been denied for CET.</td>
+                            <td>2023-09-27 10:30:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-cogs"></i></td>
+                            <td>Application Approved</td>
+                            <td>An application has been approved for CET.</td>
+                            <td>2023-09-28 13:15:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-user-cog"></i></td>
+                            <td>New Account Created</td>
+                            <td>New account created for Sarah Johnson.</td>
+                            <td>2023-09-29 11:40:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-calendar-check"></i></td>
+                            <td>Appointment Cancelled</td>
+                            <td>An appointment for CET testing has been cancelled.</td>
+                            <td>2023-09-30 09:00:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-file-alt"></i></td>
+                            <td>Online Application Updated</td>
+                            <td>Student ID 13579 has updated their online application for CET.</td>
+                            <td>2023-10-01 14:50:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-cogs"></i></td>
+                            <td>Application Review Complete</td>
+                            <td>Application review process completed for CET.</td>
+                            <td>2023-10-02 16:20:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-user-cog"></i></td>
+                            <td>Password Reset</td>
+                            <td>Password reset request for John Smith's account.</td>
+                            <td>2023-10-03 10:10:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-calendar-check"></i></td>
+                            <td>Appointment Completed</td>
+                            <td>Appointment for CET testing has been successfully completed.</td>
+                            <td>2023-10-04 13:00:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-file-alt"></i></td>
+                            <td>Online Application Reviewed</td>
+                            <td>Student ID 56789's application reviewed for CET.</td>
+                            <td>2023-10-05 11:05:00</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fas fa-cogs"></i></td>
+                            <td>Account Activated</td>
+                            <td>Account activation for Emily White's account.</td>
+                            <td>2023-10-06 14:30:00</td>
+                        </tr>
+                    </tbody>
+                </table>
                 </div>
                 <!-- Tab 3: Notification Settings -->
                 <div class="tab-pane fade" id="notificationSettings">
