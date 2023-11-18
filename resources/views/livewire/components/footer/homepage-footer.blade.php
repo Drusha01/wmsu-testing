6e8f2a5234feeb51d73297a279d00addc7d148d0
salialkhayzel <111584408+salialkhayzel@users.noreply.github.com>
2023-11-19 05:10:56 +0800

    punyeta xd

--- resources/views/livewire/components/footer/homepage-footer.blade.php
+++ resources/views/livewire/components/footer/homepage-footer.blade.php
@@ -26,10 +26,10 @@
 
         @if($footer_data)
         <section class="ewan ko lord">
-            <div class="container text-center">
+            <div class="container text-center text-md-start mt-5">
                 <div class="row mt-3">
                     @foreach ($footer_data  as $item => $value)
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-2 mt-4 mb-4">
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mt-4 mb-4">
                             <!-- Links -->
                             <h6 class="text-uppercase fw-bold mb-4">
                                 {{$value->footer_type_details}}
