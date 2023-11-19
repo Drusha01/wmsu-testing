4f9e9fb12dd46a26c4bea7346a55b101b131f9c1
salialkhayzel <111584408+salialkhayzel@users.noreply.github.com>
2023-11-19 20:10:15 +0800

    finalizatiion

--- resources/views/livewire/components/navigation/navigation-tabs.blade.php
+++ resources/views/livewire/components/navigation/navigation-tabs.blade.php
@@ -4,10 +4,10 @@
             <a class="nav-link {{request()->is('student/profile*') ? 'active' : ''}}" href="{{ route('student.profile') }}" role="tab">Profile</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{request()->is('student/requirements*') ? 'active' : ''}}" href="{{ route('student.requirements') }}" role="tab">Requirements</a>
+            <a class="nav-link {{request()->is('student/application*') ? 'active' : ''}}" href="{{ route('student.application') }}" role="tab">Application</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{request()->is('student/application*') ? 'active' : ''}}" href="{{ route('student.application') }}" role="tab">Application</a>
+            <a class="nav-link {{request()->is('student/requirements*') ? 'active' : ''}}" href="{{ route('student.requirements') }}" role="tab">Requirements</a>
         </li>
         <li class="nav-item">
             <a class="nav-link {{request()->is('student/status*') ? 'active' : ''}}" href="{{ route('student.status') }}" role="tab">Status</a>
@@ -16,10 +16,10 @@
             <a class="nav-link {{request()->is('student/results*') ? 'active' : ''}}" href="{{ route('student.results') }}" role="tab">Results</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{request()->is('student/announcement*') ? 'active' : ''}}" href="{{ route('student.announcement') }}" role="tab">Announcement</a>
+            <a class="nav-link {{request()->is('student/appointment*') ? 'active' : ''}}" href="{{ route('student.appointment') }}" role="tab">Appointments</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{request()->is('student/appointment*') ? 'active' : ''}}" href="{{ route('student.appointment') }}" role="tab">Appointments</a>
+            <a class="nav-link {{request()->is('student/announcement*') ? 'active' : ''}}" href="{{ route('student.announcement') }}" role="tab">Announcement</a>
         </li>
         <li class="nav-item">
             <a class="nav-link {{request()->is('student/chat*') ? 'active' : ''}}" href="{{ route('student.chat') }}" role="tab">Chat</a>
