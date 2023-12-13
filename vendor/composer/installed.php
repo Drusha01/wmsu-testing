7981780bdf938d53e6a8643446e2ae49fd26f334
Hanrickson E. Dumapit <114381735+Drusha01@users.noreply.github.com>
2023-12-14 05:36:23 +0800

    hostinger email update, composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd325d74a60a487f50d97e40a93eb1ee567121311',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -449,9 +449,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd325d74a60a487f50d97e40a93eb1ee567121311',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
